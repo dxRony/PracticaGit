79f3a6a109e3772d27aa6746f55340382ac6fba7
dxRony <romaroag@gmail.com>
2023-07-27 22:49:34 -0600

    prueba

--- src/main/java/com/mycompany/practicagit/PracticaGit.java
+++ src/main/java/com/mycompany/practicagit/PracticaGit.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.practicagit;
 
-/**
- *
- * @author romar
- */
+
 public class PracticaGit {
 
+    public static void main(String[] args) {
+        System.out.println("prueba");
+    }
 }
