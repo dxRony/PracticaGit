a29a139cacc1a789a76e57093de9e20fcc3305ca
dxRony <romaroag@gmail.com>
2023-07-29 01:17:48 -0600

    completando juego basket

--- src/main/java/com/mycompany/practicagit/basketball/Basketball.java
+++ src/main/java/com/mycompany/practicagit/basketball/Basketball.java
@@ -10,9 +10,10 @@ public class Basketball {
     private Scanner leer;
     private Random ran;
     private int probabilidad;
-    private int turnos;
+    private int cantidadTurnos;
+    private boolean turno;
 
-    public Basketball() {      
+    public Basketball() {
         jugador1 = new Jugador();
         jugador2 = new Jugador();
         leer = new Scanner(System.in);
@@ -21,21 +22,95 @@ public class Basketball {
 
     public void motorJuegoBasketball() {
         System.out.println("Bienvenido a Basketball");
-        
+
         System.out.println("Ingresa tu nombre Jugador 1...");
         jugador1.setNombre(leer.nextLine());
-        
-        System.out.println("Ingresa tu nombre Jugador 2...");       
+
+        System.out.println("Ingresa tu nombre Jugador 2...");
         jugador2.setNombre(leer.nextLine());
-        
-        System.out.println("Bienvenidos!!!\n" +jugador1.getNombre() + " y "+jugador2.getNombre());
+
+        System.out.println("Bienvenidos!!!\n" + jugador1.getNombre() + " y " + jugador2.getNombre() + "\n");
         generarTurnos();
-        System.out.println("Este juego consiste en simular un partido de basketbal por turnos"+"\nLa cantidad de turnos es: "+turnos);
-        
-        
+        System.out.println("Este juego consiste en simular un partido de basketball por turnos"
+                + "\nLa cantidad de turnos es: " + cantidadTurnos);
+
+        definirJugadorInicial();
+        if (turno == true) {
+            System.out.println("El jugador que inicia lanzando es: " + jugador1.getNombre());
+        } else if (turno == false) {
+            System.out.println("El jugador que inicia lanzando es: " + jugador2.getNombre());
+        }
+        System.out.println("------------------------Partida Iniciada------------------------");
+        do {
+            System.out.println("Turnos Restantes: " + cantidadTurnos);
+            if (esTurnoJugador1()) {
+                System.out.println("Es tu turno de lanzar " + jugador1.getNombre() + "\n");
+                mostrarOpcionesLanzamientoJugador();
+                int lanzamiento = leer.nextInt();
+                System.out.println("Es tu turno de defender " + jugador2.getNombre() + "\n");
+                mostrarOpcionesDefensaJugador();
+                int defensa = leer.nextInt();
+                jugador1.ejecutarLanzamiento(lanzamiento, defensa);
+            } else {
+                System.out.println("Es tu turno de lanzar " + jugador2.getNombre() + "\n");
+                mostrarOpcionesLanzamientoJugador();
+                int lanzamiento = leer.nextInt();
+                System.out.println("Es tu turno de defender " + jugador1.getNombre() + "\n");
+                mostrarOpcionesDefensaJugador();
+                int defensa = leer.nextInt();
+                jugador2.ejecutarLanzamiento(lanzamiento, defensa);
+            }
+
+            cambiarTurno();
+            cantidadTurnos--;
+            System.out.println("Puntuaciones:\n"
+                    + jugador1.getNombre() + " = " + jugador1.getPuntos() + "\n"
+                    + jugador2.getNombre() + " = " + jugador2.getPuntos() + "\n");
+        } while (cantidadTurnos > 0);
+        System.out.println("------------------------Partida Terminada------------------------");
+        if (jugador1.getPuntos() > jugador2.getPuntos()) {
+            System.out.println("Felicidades, " + jugador1.getNombre() + " ganaste");
+        } else if (jugador1.getPuntos() < jugador2.getPuntos()) {
+            System.out.println("Felicidades, " + jugador2.getNombre() + " ganaste");
+        } else if (jugador1.getPuntos() == jugador2.getPuntos()) {
+            System.out.println("Empate...");
+        }
+
+        System.out.println("Puntuaciones:\n"
+                + jugador1.getNombre() + " = " + jugador1.getPuntos() + "\n"
+                + jugador2.getNombre() + " = " + jugador2.getPuntos() + "\n");
     }
-    
+
     private void generarTurnos() {
-        turnos = ran.nextInt(5,20);
+        cantidadTurnos = ran.nextInt(5, 20);
+    }
+
+    private void definirJugadorInicial() {
+        int val = ran.nextInt(100);
+        if (val <= 50) {
+            turno = true;
+        } else if (val >= 50) {
+            turno = false;
+        }
+    }
+
+    private void mostrarOpcionesLanzamientoJugador() {
+        System.out.println("Elige uno de los siguientes lanzamientos"
+                + "\n[1] Salto largo desde 5 metros, vale 3 puntos y la probabilidad de anotar es de 65%"
+                + "\n[2] Salto corto desde 3 metros, vale 2 puntos y la probabilidad de anotar es de 80%");
+    }
+
+    private void mostrarOpcionesDefensaJugador() {
+        System.out.println("Elige uno de los siguientes movimientos de defensa"
+                + "\n[1] Defensa cuerpo a cuerpo, reduce la probabilidad de anotar en un 15% y la probabilidad de hacer falta del 35%"
+                + "\n[2] Defensa fuerte, reduce la probabildad de anotar en un 30% y la probabilidad de hacer falta del 65%");
+    }
+
+    private boolean esTurnoJugador1() {
+        return turno;
+    }
+
+    private void cambiarTurno() {
+        turno = !turno;
     }
 }
--- src/main/java/com/mycompany/practicagit/basketball/Jugador.java
+++ src/main/java/com/mycompany/practicagit/basketball/Jugador.java
@@ -1,12 +1,127 @@
 package com.mycompany.practicagit.basketball;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Jugador {
 
     private String nombre;
     private int puntos;
-    private boolean turno;
+    private int probabilidadAnotar;
+    private int probabilidadFalta;
+    private int probabilidadTiroLibre;
+    private Random ran;
+    private Scanner leer;
 
     public Jugador() {
+        ran = new Random();
+        leer = new Scanner(System.in);
+    }
+
+    public void ejecutarLanzamiento(int lanzamiento, int defensa) {
+        generarProbabilidadAnotar();
+        generarProbabilidadFalta();
+        generarProbabilidadTiroLibre();
+
+        if (lanzamiento == 1 && defensa == 1) {
+            probabilidadAnotar -= 15;
+            if (probabilidadFalta <= 35) {
+                System.out.println("¡FALTA!\nEl jugador tiene un tiro libre...");
+                System.out.println("Preparando para ejecutar el tiro...(vale 2 puntos y la probabilidad de anotar es de 90%)"
+                        + "\nPresiona enter para continuar");
+                leer.nextLine();
+                if (probabilidadTiroLibre >= 10) {
+                    System.out.println("¡ANOTACIONNN!");
+                    System.out.println("Jugador lanzador +2 puntos");
+                    this.puntos += 2;
+                } else {
+                    System.out.println("Tiro libre FALLADO");
+                }
+            } else if (probabilidadAnotar <= 50) {
+                System.out.println("¡ANOTACIONNN!");
+                System.out.println("Jugador lanzador +3 puntos");
+                this.puntos += 3;
+            } else if (probabilidadAnotar > 50) {
+                System.out.println("Se bloqueo el lanzamiento");
+            }
+        } else if (lanzamiento == 1 && defensa == 2) {
+            probabilidadAnotar -= 30;
+            if (probabilidadFalta <= 65) {
+                System.out.println("¡FALTA!\nEl jugador tiene un tiro libre...");
+                System.out.println("Preparando para ejecutar el tiro...(vale 2 puntos y la probabilidad de anotar es de 90%)"
+                        + "\nPresiona enter para continuar");
+                leer.nextLine();
+                if (probabilidadTiroLibre >= 10) {
+                    System.out.println("¡ANOTACIONNN!");
+                    System.out.println("Jugador lanzador +2 puntos");
+                    this.puntos += 2;
+                } else {
+                    System.out.println("Tiro libre FALLADO");
+                }
+            } else if (probabilidadAnotar <= 30) {
+                System.out.println("¡ANOTACIONNN!");
+                System.out.println("Jugador lanzador +3 puntos");
+                this.puntos += 3;
+            } else if (probabilidadAnotar > 30) {
+                System.out.println("Se bloqueo el lanzamiento");
+            }
+        } else if (lanzamiento == 2 && defensa == 1) {
+            probabilidadAnotar -= 15;
+            if (probabilidadFalta <= 35) {
+                System.out.println("¡FALTA!\nEl jugador tiene un tiro libre...");
+                System.out.println("Preparando para ejecutar el tiro...(vale 2 puntos y la probabilidad de anotar es de 90%)"
+                        + "\nPresiona enter para continuar");
+                leer.nextLine();
+                if (probabilidadTiroLibre >= 10) {
+                    System.out.println("¡ANOTACIONNN!");
+                    System.out.println("Jugador lanzador +2 puntos");
+                    this.puntos += 2;
+                } else {
+                    System.out.println("Tiro libre FALLADO");
+                }
+            } else if (probabilidadAnotar <= 65) {
+                System.out.println("¡ANOTACIONNN!");
+                System.out.println("Jugador lanzador +2 puntos");
+                this.puntos += 2;
+            } else if (probabilidadAnotar > 65) {
+                System.out.println("Se bloqueo el lanzamiento");
+            }
+        } else if (lanzamiento == 2 && defensa == 2) {
+            probabilidadAnotar -= 30;
+            if (probabilidadFalta <= 65) {
+                System.out.println("¡FALTA!\nEl jugador tiene un tiro libre...");
+                System.out.println("Preparando para ejecutar el tiro...(vale 2 puntos y la probabilidad de anotar es de 90%)"
+                        + "\nPresiona enter para continuar");
+                leer.nextLine();
+                if (probabilidadTiroLibre >= 10) {
+                    System.out.println("¡ANOTACIONNN!");
+                    System.out.println("Jugador lanzador +2 puntos");
+                    this.puntos += 2;
+                } else {
+                    System.out.println("Tiro libre FALLADO");
+                }
+            } else if (probabilidadAnotar <= 50) {
+                System.out.println("¡ANOTACIONNN!");
+                System.out.println("Jugador lanzador +2 puntos");
+                this.puntos += 2;
+            } else if (probabilidadAnotar > 50) {
+                System.out.println("Se bloqueo el lanzamiento");
+            }
+        } else {
+            System.out.println("Se ingresaron valores incorrectos...");
+        }
+    }
+
+    private void generarProbabilidadAnotar() {
+        probabilidadAnotar = ran.nextInt(100);
+    }
+
+    private void generarProbabilidadFalta() {
+        probabilidadFalta = ran.nextInt(100);
+    }
+
+    private void generarProbabilidadTiroLibre() {
+        probabilidadTiroLibre = ran.nextInt(100);
     }
 
     public String getNombre() {
@@ -24,13 +139,4 @@ public class Jugador {
     public void setPuntos(int puntos) {
         this.puntos = puntos;
     }
-
-    public boolean isTurno() {
-        return turno;
-    }
-
-    public void setTurno(boolean turno) {
-        this.turno = turno;
-    }
-
 }
